d93cb2b528c89731ebd15c0d3245a203ebb97cc3
balochCoder <raheelahmed017@gmail.com>
2022-06-13 16:04:02 +0500

    Notification Scheduling

---
+++ app/Console/Commands/SendDueReservationsNotifications.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Reservation;
+use App\Notifications\HostReservationStarting;
+use App\Notifications\NewUserReservation;
+use App\Notifications\UserReservationStarting;
+use Illuminate\Console\Command;
+use Notification;
+
+class SendDueReservationsNotifications extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ergodnc:reservations-notifications';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Reservation::query()
+            ->with('office.user')
+            ->where('status', Reservation::STATUS_ACTIVE)
+            ->where('start_date', now()->toDateString())
+            ->each(function ($reservation) {
+                Notification::send($reservation->user, new UserReservationStarting($reservation));
+
+                Notification::send($reservation->office->user, new HostReservationStarting($reservation));
+            });
+        return 0;
+    }
+}
